63ef591268ea5512ce6ec00bd07a53e9a566fe35
Evander Perez <majicore@hotmail.com>
2023-06-05 14:11:24 -0600

    feat(Task): Update and Destroy methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   def index
     pagy, tasks = pagy(
-      TaskService.new(nil, filter_params).index
+      TaskService.new(nil, filter_params, nil).index
     )
 
     render json: paginate_response_with_serializer(pagy, tasks, TaskSerializer)
@@ -12,30 +12,30 @@ class TasksController < ApplicationController
 
   # GET /tasks/:id
   def show
-    task = TaskService.new(nil, params).show
+    task = TaskService.new(nil, nil, params[:id]).show
 
     render json: task, serializer: TaskSerializer
   end
 
   # POST /tasks/:id
   def create
-    task = TaskService.new(nil, task_params).create
+    task = TaskService.new(nil, task_params, nil).create
 
     render json: task, serializer: TaskSerializer
   end
 
   # PATCH/PUT /tasks/1
   def update
-    if @task.update(task_params)
-      render json: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
+    task = TaskService.new(nil, task_params, params[:id]).update
+
+    render json: task, serializer: TaskSerializer
   end
 
   # DELETE /tasks/1
   def destroy
-    @task.destroy
+    result = TaskService.new(nil, nil, params[:id]).destroy
+
+    render json: { success: result }
   end
 
   private
--- app/services/task_service.rb
+++ app/services/task_service.rb
@@ -1,7 +1,9 @@
 class TaskService
-  def initialize(user, params)
+  def initialize(user, params, task_id = nil)
     @current_user = user
     @params = params
+    @task_id = task_id || params[:id]
+    @task =  Task.find(@task_id) if @task_id
   end
 
   def index
@@ -12,10 +14,20 @@ class TaskService
   end
 
   def show
-    Task.find(@params[:id])
+    @task
   end
 
   def create
     Task.create(@params)
+  end
+
+  def update
+    @task.update!(@params)
+    @task
+  end
+
+  def destroy
+    @task.destroy
+    @task.destroyed?
   end
 end
